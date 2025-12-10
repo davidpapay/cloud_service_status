ecd3d03486c5fafc75c53e4162ee74a8f25eca29
David Papay <papaydavid@seznam.cz>
2025-12-10 15:24:46 +0100

    chore: add Azure Container Registry in main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -5,3 +5,13 @@ resource "azurerm_resource_group" "cloud_service_status_rg" {
   name     = "rg-cloud-service-status-dev"
   location = "westeurope"
 }
+
+# Azure Container Registry
+resource "azurerm_container_registry" "cloud_service_status_acr" {
+  name                = "acrcloudservicestatusdev"
+  resource_group_name = azurerm_resource_group.cloud_service_status_rg.name
+  location            = azurerm_resource_group.cloud_service_status_rg.location
+  sku                 = "Basic"
+
+  admin_enabled       = true
+}

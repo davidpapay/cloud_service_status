ec1ee1beb3e215ad6e29203815cf1988d6b0b5e3
David Papay <papaydavid@seznam.cz>
2025-12-15 23:16:58 +0100

    chore: restructure Terraform infra into dev and prod directories

---
+++ infra/prod/provider.tf
@@ -0,0 +1,16 @@
+# Terraform configuration for Azure Resource Manager
+
+terraform {
+  required_version = ">= 1.14.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}

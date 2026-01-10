c3e271d525d6bda45e6769d347e0fdecf633db7c
David Papay <papaydavid@seznam.cz>
2026-01-10 19:31:36 +0100

    chore: restructure Terraform infra modues

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -1,15 +1,16 @@
 # Main Terraform configuration file - Development environment
 
-# Azure resource group
+# Azure Resource Group
 resource "azurerm_resource_group" "cloud_service_status_rg" {
   name     = "rg-cloud-service-status-dev"
   location = "westeurope"
 }
 
-# Azure Container Registry module instance
-module "cloud_service_status_acr" {
+# Azure Container App module instance
+module "cloud_service_status" {
   source              = "../modules/container_app"
-  name                = "acrcloudservicestatusdev"
+  name                = "cloud-service-status-dev"
   resource_group_name = azurerm_resource_group.cloud_service_status_rg.name
   location            = azurerm_resource_group.cloud_service_status_rg.location
+  container_image     = "acrcloudservicestatusdev.azurecr.io/cloud-service-status:latest"
 }
--- infra/modules/container_app/variables.tf
+++ infra/modules/container_app/variables.tf
@@ -1,5 +1,25 @@
-# Variables for Container App Module
+variable "name" {
+  description = "Base name for all resources"
+}
 
-variable "name" {}
-variable "resource_group_name" {}
-variable "location" {}
+variable "resource_group_name" {
+  description = "Resource group where all resources are deployed"
+}
+
+variable "location" {
+  description = "Azure region"
+}
+
+variable "container_image" {
+  description = "Docker image for the Container App"
+}
+
+variable "cpu" {
+  description = "CPU for container"
+  default     = 0.25
+}
+
+variable "memory" {
+  description = "Memory for container"
+  default     = "0.5Gi"
+}

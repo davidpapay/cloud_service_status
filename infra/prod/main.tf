0cea39679d9d38b9713caea23dc5827222ba027e
David Papay <papaydavid@seznam.cz>
2026-01-10 19:31:36 +0100

    chore: temporarily disable prod Terraform resources to reduce Azure costs

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -1,16 +1,16 @@
 # Main Terraform configuration file - Production environment
-
+# Prod resources temporairly disabled to reduce Azure costs
 # Azure resource group
-resource "azurerm_resource_group" "cloud_service_status_rg" {
-  name     = "rg-cloud-service-status-prod"
-  location = "westeurope"
-}
+#resource "azurerm_resource_group" "cloud_service_status_rg" {
+#  name     = "rg-cloud-service-status-prod"
+#  location = "westeurope"
+#}
 
 # Azure Container Registry module instance
-module "cloud_service_status_acr" {
-  source              = "../modules/container_app"
-  name                = "acrcloudservicestatusprod"
-  resource_group_name = azurerm_resource_group.cloud_service_status_rg.name
-  location            = azurerm_resource_group.cloud_service_status_rg.location
-  container_image     = "acrcloudservicestatusprod.azurecr.io/cloud-service-status:latest"
-}
+#module "cloud_service_status_acr" {
+#  source              = "../modules/container_app"
+#  name                = "acrcloudservicestatusprod"
+#  resource_group_name = azurerm_resource_group.cloud_service_status_rg.name
+#  location            = azurerm_resource_group.cloud_service_status_rg.location
+#  container_image     = "acrcloudservicestatusprod.azurecr.io/cloud-service-status:latest"
+#}

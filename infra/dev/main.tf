eeb362c94229955bc5c082770c91281fe17ef3db
David Papay <papaydavid@seznam.cz>
2026-02-02 16:52:44 +0100

    chore: simplify container image delivery and reduce cost by removing ACR and using GHCR

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -6,14 +6,6 @@ resource "azurerm_resource_group" "this" {
   location = "westeurope"
 }
 
-# Azure ACR module instance
-module "acr" {
-  source              = "../modules/acr"
-  name                = "acrcloudservicestatusdev"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-}
-
 # Azure Container App module instance
 module "app" {
   source              = "../modules/container_app"
@@ -21,7 +13,4 @@ module "app" {
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   container_image     = "acrcloudservicestatusdev.azurecr.io/cloud-service-status:latest"
-  registry_server   = module.acr.login_server
-  registry_username = module.acr.username
-  registry_password = module.acr.password
 }
--- infra/modules/container_app/main.tf
+++ infra/modules/container_app/main.tf
@@ -22,17 +22,6 @@ resource "azurerm_container_app" "this" {
   container_app_environment_id = azurerm_container_app_environment.this.id
   revision_mode                = "Single"
   
-  # ACR configuration
-  secret {
-    name  = "acr-password"
-    value = var.registry_password
-  }
-  registry {
-    server               = var.registry_server
-    username             = var.registry_username
-    password_secret_name = "acr-password"
-  }
-
   # Container configuration
   template {
     container {
